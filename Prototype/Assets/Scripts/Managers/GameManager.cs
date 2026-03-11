57878f315d6f552bd6806c57f59008264a7940f5
melayerjohn <johnnbhore@gmail.com>
2026-03-11 16:12:28 +0530

    Add ScoreManager and integrate scoring with match system

--- Prototype/Assets/Scripts/Managers/GameManager.cs
+++ Prototype/Assets/Scripts/Managers/GameManager.cs
@@ -8,6 +8,9 @@ public class GameManager : MonoBehaviour
 
     private List<Card> revealCards = new List<Card>();
 
+    private Card firstCard;
+    private Card secondCard;
+
     private void Awake()
     {
         Instance = this;
@@ -15,28 +18,53 @@ public class GameManager : MonoBehaviour
 
     public void CardRevealed(Card _card)
     {
-        revealCards.Add(_card);
-        if(revealCards.Count==2)
+        if (firstCard == null)
         {
-            StartCoroutine(CheckMatch());
+            firstCard = _card;
+            return;
         }
+
+        if (secondCard == null)
+        {
+            secondCard = _card;
+
+            StartCoroutine(CheckMatch(firstCard, secondCard));
+
+            firstCard = null;
+            secondCard = null;
+        }
+
+
+        //revealCards.Add(_card);
+        //if(revealCards.Count==2)
+        //{
+        //    Card card1 = revealCards[0];
+        //    Card card2 = revealCards[1];
+
+        //    revealCards.RemoveAt(0);
+        //    revealCards.RemoveAt(0);
+
+        //    StartCoroutine(CheckMatch(card1, card2));
+        //}
     }
 
-    IEnumerator CheckMatch()
+    IEnumerator CheckMatch(Card card1, Card card2)
     {
-        Card card1 = revealCards[0];
-        Card card2 = revealCards[1];
         yield return new WaitForSeconds(0.5f);
 
         if (card1.cardId == card2.cardId)
         {
             card1.SetMatched();
             card2.SetMatched();
+
+            ScoreManager.Instance.AddMatchScore();
         }
         else
         {
             card1.FlipBack();
             card2.FlipBack();
+
+            ScoreManager.Instance.AddMismatchPenalty();
         }
 
         revealCards.Clear();
--- Prototype/Assets/Scripts/Managers/ScoreManager.cs
+++ Prototype/Assets/Scripts/Managers/ScoreManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static ScoreManager Instance;
+
+    public int score;
+
+    public int matchPoints = 10;
+    public int mismatchPenalty = -2;
+
+    public TMP_Text scoreText;
+
+    [SerializeField]private bool isAddMismatchPenalty;
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddMatchScore()
     {
-        
+        score += matchPoints;
+        Debug.Log("Score: " + score);
+        UpdateScoreUI();
+    }
+
+    public void AddMismatchPenalty()
+    {
+        if (isAddMismatchPenalty)
+        {
+            score += mismatchPenalty;
+            Debug.Log("Score: " + score);
+            UpdateScoreUI();
+        }
+    }
+
+    private void UpdateScoreUI()
+    {
+        scoreText.text = score.ToString();
     }
 }
